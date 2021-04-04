1dc8e17a5fc7792773f99c40f88c76300dd81f0e
fercell122 <ortyus@outlook.com>
2021-04-04 17:39:10 -0300

    test3

--- src/components/videoloop/videoloop.js
+++ src/components/videoloop/videoloop.js
@@ -11,7 +11,7 @@ export default function videlop(){
             "width":"100%",
             "height":"100%",
              }}>
-            <source src={process.env.PUBLIC_URL + '/video/fl.mp4'} type="video/mp4"/>
+            <source src="https://videolopp.s3.amazonaws.com/fl.mp4" type="video/mp4"/>
         </video>
     )
 }
