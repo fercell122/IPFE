c86d939c96d9b1fdc1934e8e3115ae1b68019729
fercell122 <ortyus@outlook.com>
2021-04-07 15:18:37 -0300

    test5

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -8,6 +8,7 @@ import GridItem from "../../components/Grid/GridItem.js";
 import image from "../../assets/images/bg2.jpg"
 import VideoL from "../../components/videoloop/videoloop.js"
 
+
 const useStyles = makeStyles(styles);
 
 
@@ -35,6 +36,22 @@ export default function Landing(){
       </Parallax>
       <div className={classNames(classes.main, classes.mainRaised)}>
       <VideoL/>
+      <h1
+      style={{
+        textAlign:"center"
+      }}>
+      Apresentação da Matéria de TI
+      </h1>
+      <h2
+      style={{
+        textAlign:"center"
+      }}>
+        Tema: Manutenção Corretiva e Preventiva
+      </h2>
+      <video 
+        controls>
+            <source src="https://videolopp.s3.amazonaws.com/79SB7D1ZSV0N0XG7.mp4" type="video/mp4"/>
+        </video>
       </div>
       </div>
     )
